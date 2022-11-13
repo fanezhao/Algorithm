5549269bbc9fd924c34534806f8908b6a1fd823e
fanezhao <fanezhao@gmail.com>
2022-11-13 16:49:53 +0800

    添加快排的单边循环实现和非递归实现

--- src/com/zmoyi/algorithm/sort/QuickSort.java
+++ src/com/zmoyi/algorithm/sort/QuickSort.java
@@ -1,12 +1,21 @@
 package com.zmoyi.algorithm.sort;
 
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Stack;
 
 /**
  * 快速排序
  */
 public class QuickSort {
 
+    /**
+     * 快排：递归实现
+     * @param array
+     * @param startIndex
+     * @param endIndex
+     */
     public static void sort(int[] array, int startIndex, int endIndex) {
         // 递归结束条件：startIndex大于或等于endIndex
         if (startIndex >= endIndex) {
@@ -14,14 +23,14 @@ public class QuickSort {
         }
 
         // 获取基准元素位置
-        int pivotIndex = partition(array, startIndex, endIndex);
+        int pivotIndex = partition2(array, startIndex, endIndex);
         // 根据基准元素，分两部分进行递归
         sort(array, startIndex, pivotIndex - 1);
         sort(array, pivotIndex + 1, endIndex);
     }
 
     /**
-     * 双边循环
+     * 分治：双边循环
      * @param array
      * @param startIndex
      * @param endIndex
@@ -57,9 +66,69 @@ public class QuickSort {
         return left;
     }
 
+    /**
+     * 分治：单边循环
+     * @param array
+     * @param startIndex
+     * @param endIndex
+     * @return
+     */
+    public static int partition2(int[] array, int startIndex, int endIndex) {
+        int pivot = array[startIndex];
+        int mark = startIndex;
+
+        for (int i = startIndex + 1; i <= endIndex; i++) {
+            if (array[i] < pivot) {
+                mark++;
+                int tmp = array[i];
+                array[i] = array[mark];
+                array[mark] = tmp;
+            }
+        }
+
+        array[startIndex] = array[mark];
+        array[mark] = pivot;
+        return mark;
+    }
+
+    /**
+     * 快排：非递归实现
+     * @param arr
+     * @param startIndex
+     * @param endIndex
+     */
+    public static void sort2(int[] arr, int startIndex, int endIndex) {
+        // 用一个集合栈来代替递归的函数栈
+        Stack<Map<String, Integer>> quickSortStack = new Stack<>();
+        // 整个数列的起始下标，以哈希的形式入栈
+        Map<String, Integer> rootParam = new HashMap<>();
+        rootParam.put("startIndex", startIndex);
+        rootParam.put("endIndex", endIndex);
+        quickSortStack.push(rootParam);
+        // 循环结束条件：栈为空时
+        while (!quickSortStack.isEmpty()) {
+            Map<String, Integer> param = quickSortStack.pop();
+            int pivotIndex = partition(arr, param.get("startIndex"), param.get("endIndex"));
+
+            if (param.get("startIndex") < pivotIndex - 1) {
+                Map<String, Integer> leftParam = new HashMap<>();
+                leftParam.put("startIndex", startIndex);
+                leftParam.put("endIndex", pivotIndex - 1);
+                quickSortStack.push(leftParam);
+            }
+
+            if (param.get("endIndex") > pivotIndex + 1) {
+                Map<String, Integer> rightParam = new HashMap<>();
+                rightParam.put("startIndex", pivotIndex + 1);
+                rightParam.put("endIndex", endIndex);
+                quickSortStack.push(rightParam);
+            }
+        }
+    }
+
     public static void main(String[] args) {
         int[] arr = new int[]{4, 4, 6, 5, 3, 2, 8, 1};
-        sort(arr, 0, arr.length - 1);
+        sort2(arr, 0, arr.length - 1);
         System.out.println(Arrays.toString(arr));
     }
 
