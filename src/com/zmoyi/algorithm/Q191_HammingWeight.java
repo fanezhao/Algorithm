9aa9e7dec853d00851c3212e59bb4f8c5aedbf7b
fanezhao <fanezhao@gmail.com>
2022-11-15 18:27:42 +0800

    添加：231判断一个数是否是2的次幂；191检查一个数二进制位中1个的个数

---
+++ src/com/zmoyi/algorithm/Q191_HammingWeight.java
@@ -0,0 +1,39 @@
+package com.zmoyi.algorithm;
+
+public class Q191_HammingWeight {
+
+    /*
+
+    编写一个函数，输入是一个无符号整数（以二进制串的形式），返回其二进制表达式中数字位数为 '1' 的个数（也被称为汉明重量）。
+
+     */
+
+    public static void main(String[] args) {
+
+        System.out.println(hammingWeight2(13));
+    }
+
+    /**
+     * 直接循环检查给定整数 n 的二进制位的每一位是否为 1
+     * @param num
+     * @return
+     */
+    public static int hammingWeight(int num) {
+        int count = 0;
+        for (int i = 0; i < 32; i++) {
+            if ((num & (1 << i)) != 0) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    public static int hammingWeight2(int num) {
+        int count = 0;
+        while (num != 0) {
+            num = num & (num - 1);
+            count ++;
+        }
+        return count;
+    }
+}
--- src/com/zmoyi/algorithm/array/Q1979_FindGCD.java
+++ src/com/zmoyi/algorithm/array/Q1979_FindGCD.java
@@ -48,7 +48,7 @@ public class Q1979_FindGCD {
             }
         }
 
-        return getGreatestCommonDivisorV2(max, min);
+        return getGreatestCommonDivisorV3(max, min);
     }
 
     /**
@@ -75,7 +75,7 @@ public class Q1979_FindGCD {
     }
 
     /**
-     * 辗转相除法（欧几里得算法）：两个正整数a和b（a > b），它们的最大公约数等于a除以b的余数c和b的约数。
+     * 辗转相除法（欧几里得算法）：两个正整数a和b（a > b），它们的最大公约数等于a除以b的余数c和b的最大公约数。
      * 比如25和10的约数，等于25除以10的余数5和10的最大公约数，即为5。
      * @param a
      * @param b
@@ -90,6 +90,23 @@ public class Q1979_FindGCD {
         }
 
         return getGreatestCommonDivisorV2(big % small, small);
+    }
+
+    /**
+     * 更相减损术（九章算术）：两个正整数a和b（a > b），它们的最大公约数等于a减去b的余差值c和较小的b的最大公约数。
+     * @param a
+     * @param b
+     * @return
+     */
+    public int getGreatestCommonDivisorV3(int a, int b) {
+        int big = a > b ? a : b;
+        int small = a < b ? a : b;
+
+        if (big - small == 0) {
+            return small;
+        }
+
+        return getGreatestCommonDivisorV3(big % small, small);
     }
 
     public static void main(String[] args) {
