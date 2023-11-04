0de1f588cc8788faab84c58e5a4ddbed33c68987
fanezhao <fanezhao@gmail.com>
2023-11-04 18:50:38 +0800

    mod: 二叉树的最大深度

---
+++ src/com/zmoyi/leetcode/editor/cn/LC_104_二叉树的最大深度.java
@@ -0,0 +1,99 @@
+//给定一个二叉树 root ，返回其最大深度。 
+//
+// 二叉树的 最大深度 是指从根节点到最远叶子节点的最长路径上的节点数。 
+//
+// 
+//
+// 示例 1： 
+//
+// 
+//
+// 
+//
+// 
+//输入：root = [3,9,20,null,null,15,7]
+//输出：3
+// 
+//
+// 示例 2： 
+//
+// 
+//输入：root = [1,null,2]
+//输出：2
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 树中节点的数量在 [0, 10⁴] 区间内。 
+// -100 <= Node.val <= 100 
+// 
+//
+// Related Topics 树 深度优先搜索 广度优先搜索 二叉树 👍 1734 👎 0
+
+
+package com.zmoyi.leetcode.editor.cn;
+
+import com.zmoyi.TreeNode;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class LC_104_二叉树的最大深度 {
+    public static void main(String[] args) {
+        Solution solution = new LC_104_二叉树的最大深度().new Solution();
+    }
+    //leetcode submit region begin(Prohibit modification and deletion)
+
+    /**
+     * Definition for a binary tree node.
+     * public class TreeNode {
+     * int val;
+     * TreeNode left;
+     * TreeNode right;
+     * TreeNode() {}
+     * TreeNode(int val) { this.val = val; }
+     * TreeNode(int val, TreeNode left, TreeNode right) {
+     * this.val = val;
+     * this.left = left;
+     * this.right = right;
+     * }
+     * }
+     */
+    class Solution {
+
+        /**
+         * 二叉树层序遍历
+         *
+         * @param root
+         * @return
+         */
+        public int maxDepth(TreeNode root) {
+            int depth = 0;
+            if (root == null) {
+                return depth;
+            }
+            Queue<TreeNode> queue = new LinkedList<>();
+            queue.offer(root);
+            while (!queue.isEmpty()) {
+                int size = queue.size();
+                while (size > 0) {
+                    TreeNode node = queue.poll();
+                    if (node.left != null) {
+                        queue.offer(node.left);
+                    }
+                    if (node.right != null) {
+                        queue.offer(node.right);
+                    }
+                    size--;
+                }
+                depth++;
+            }
+            return depth;
+        }
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
