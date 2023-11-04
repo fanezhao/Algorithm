cd51261e86377668ad8458efe18206bb85953424
fanezhao <fanezhao@gmail.com>
2023-11-04 17:10:12 +0800

    mod: 二叉树的层序遍历 II

---
+++ src/com/zmoyi/leetcode/editor/cn/LC_107_二叉树的层序遍历2.java
@@ -0,0 +1,105 @@
+//给你二叉树的根节点 root ，返回其节点值 自底向上的层序遍历 。 （即按从叶子节点所在层到根节点所在的层，逐层从左向右遍历） 
+//
+// 
+//
+// 示例 1： 
+// 
+// 
+//输入：root = [3,9,20,null,null,15,7]
+//输出：[[15,7],[9,20],[3]]
+// 
+//
+// 示例 2： 
+//
+// 
+//输入：root = [1]
+//输出：[[1]]
+// 
+//
+// 示例 3： 
+//
+// 
+//输入：root = []
+//输出：[]
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 树中节点数目在范围 [0, 2000] 内 
+// -1000 <= Node.val <= 1000 
+// 
+//
+// Related Topics 树 广度优先搜索 二叉树 👍 746 👎 0
+
+
+package com.zmoyi.leetcode.editor.cn;
+
+import com.zmoyi.TreeNode;
+
+import java.util.*;
+
+public class LC_107_二叉树的层序遍历2 {
+    public static void main(String[] args) {
+        Solution solution = new LC_107_二叉树的层序遍历2().new Solution();
+    }
+    //leetcode submit region begin(Prohibit modification and deletion)
+
+    /**
+     * Definition for a binary tree node.
+     * public class TreeNode {
+     * int val;
+     * TreeNode left;
+     * TreeNode right;
+     * TreeNode() {}
+     * TreeNode(int val) { this.val = val; }
+     * TreeNode(int val, TreeNode left, TreeNode right) {
+     * this.val = val;
+     * this.left = left;
+     * this.right = right;
+     * }
+     * }
+     */
+    class Solution {
+
+        /**
+         * 二叉树层序遍历之后反转一下结果集即可
+         * @param root
+         * @return
+         */
+        public List<List<Integer>> levelOrderBottom(TreeNode root) {
+            List<List<Integer>> ret = new ArrayList<>();
+            if (root == null) {
+                return ret;
+            }
+            levelBottom(root, ret);
+            return ret;
+        }
+
+        private void levelBottom(TreeNode root, List<List<Integer>> ret) {
+            Queue<TreeNode> queue = new LinkedList<>();
+            queue.offer(root);
+            while (!queue.isEmpty()) {
+                int size = queue.size();
+                List<Integer> levelList = new ArrayList<>();
+                while (size > 0) {
+                    TreeNode node = queue.poll();
+                    levelList.add(node.val);
+                    if (node.left != null) {
+                        queue.offer(node.left);
+                    }
+                    if (node.right != null) {
+                        queue.offer(node.right);
+                    }
+                    size--;
+                }
+                ret.add(levelList);
+            }
+            Collections.reverse(ret);
+        }
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
