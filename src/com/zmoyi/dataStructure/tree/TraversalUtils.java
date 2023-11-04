f86b3455d5882d60a6a362d18f4cccc9ba71e346
fanezhao <fanezhao@gmail.com>
2023-11-04 08:47:58 +0800

    mod: 二叉树迭代版遍历

--- src/com/zmoyi/dataStructure/tree/TraversalUtils.java
+++ src/com/zmoyi/dataStructure/tree/TraversalUtils.java
@@ -2,8 +2,8 @@ package com.zmoyi.dataStructure.tree;
 
 import com.zmoyi.TreeNode;
 
-import java.util.LinkedList;
-import java.util.Queue;
+import java.util.*;
+import java.util.stream.Collectors;
 
 /**
  * 树的遍历
@@ -14,64 +14,235 @@ import java.util.Queue;
 public class TraversalUtils {
 
     /**
-     * 前序遍历
+     * 前序遍历（递归版）
      *
      * @param root
      */
     public static void preOrder(TreeNode root) {
         if (root == null) {
+            System.out.println("null");
             return;
         }
-        System.out.println(root.val);
-        preOrder(root.left);
-        preOrder(root.right);
+        List<Integer> ret = new ArrayList<>();
+        pre(root, ret);
+        System.out.println(ret.stream().map(String::valueOf).collect(Collectors.joining(", ")));
+    }
+
+    private static void pre(TreeNode root, List<Integer> ret) {
+        if (root == null) {
+            return;
+        }
+        ret.add(root.val);
+        pre(root.left, ret);
+        pre(root.right, ret);
     }
 
     /**
-     * 中序遍历
+     * 前序遍历（迭代版）
+     *
+     * @param root
+     */
+    public static void preOrderWithStack(TreeNode root) {
+        if (root == null) {
+            System.out.println("null");
+            return;
+        }
+        List<Integer> ret = new ArrayList<>();
+        preWithStack(root, ret);
+        System.out.println(ret.stream().map(String::valueOf).collect(Collectors.joining(", ")));
+    }
+
+    private static void preWithStack(TreeNode root, List<Integer> ret) {
+        Stack<TreeNode> stack = new Stack<>();
+        stack.push(root);
+        while (!stack.isEmpty()) {
+            TreeNode node = stack.pop();
+            ret.add(node.val);
+            if (node.right != null) {
+                stack.push(node.right);
+            }
+            if (node.left != null) {
+                stack.push(node.left);
+            }
+        }
+    }
+
+    /**
+     * 中序遍历（递归版）
      *
      * @param root
      */
     public static void inOrder(TreeNode root) {
         if (root == null) {
+            System.out.println("null");
             return;
         }
-        inOrder(root.left);
-        System.out.println(root.val);
-        inOrder(root.right);
+        List<Integer> ret = new ArrayList<>();
+        in(root, ret);
+        System.out.println(ret.stream().map(String::valueOf).collect(Collectors.joining(", ")));
+    }
+
+    private static void in(TreeNode root, List<Integer> ret) {
+        if (root == null) {
+            return;
+        }
+        in(root.left, ret);
+        ret.add(root.val);
+        in(root.right, ret);
     }
 
     /**
-     * 后续遍历
+     * 中序遍历（迭代版）
+     *
+     * @param root
+     */
+    public static void inOrderWithStack(TreeNode root) {
+        if (root == null) {
+            System.out.println("null");
+            return;
+        }
+        List<Integer> ret = new ArrayList<>();
+        inWithStack(root, ret);
+        System.out.println(ret.stream().map(String::valueOf).collect(Collectors.joining(", ")));
+    }
+
+    private static void inWithStack(TreeNode root, List<Integer> ret) {
+        Stack<TreeNode> stack = new Stack<>();
+        TreeNode node = root;
+        while (node != null || !stack.isEmpty()) {
+            while (node != null) {
+                stack.push(node);
+                node = node.left;
+            }
+            if (!stack.isEmpty()) {
+                node = stack.pop();
+                ret.add(node.val);
+                node = node.right;
+            }
+        }
+    }
+
+    /**
+     * 后续遍历（递归版）
      *
      * @param root
      */
     public static void postOrder(TreeNode root) {
         if (root == null) {
+            System.out.println("null");
             return;
         }
-        postOrder(root.left);
-        postOrder(root.right);
-        System.out.println(root.val);
+        List<Integer> ret = new ArrayList<>();
+        post(root, ret);
+        System.out.println(ret.stream().map(String::valueOf).collect(Collectors.joining(", ")));
+    }
+
+    private static void post(TreeNode root, List<Integer> ret) {
+        if (root == null) {
+            return;
+        }
+        post(root.left, ret);
+        post(root.right, ret);
+        ret.add(root.val);
     }
 
     /**
-     * 层序遍历
+     * 后续遍历（迭代版）
+     *
+     * @param root
+     */
+    public static void postOrderWithStack(TreeNode root) {
+        if (root == null) {
+            System.out.println("null");
+            return;
+        }
+        List<Integer> ret = new ArrayList<>();
+        postWithStack(root, ret);
+        System.out.println(ret.stream().map(String::valueOf).collect(Collectors.joining(", ")));
+    }
+
+    private static void postWithStack(TreeNode root, List<Integer> ret) {
+        Stack<TreeNode> stack = new Stack<>();
+        stack.push(root);
+        while (!stack.isEmpty()) {
+            TreeNode node = stack.pop();
+            ret.add(node.val);
+            if (node.left != null) {
+                stack.push(node.left);
+            }
+            if (node.right != null) {
+                stack.push(node.right);
+            }
+        }
+        Collections.reverse(ret);
+    }
+
+    /**
+     * 层序遍历（迭代法）
      *
      * @param root
      */
     public static void levelOrder(TreeNode root) {
+        if (root == null) {
+            System.out.println("null");
+            return;
+        }
+        List<Integer> ret = new ArrayList<>();
+        level(root, ret);
+        System.out.println(ret.stream().map(String::valueOf).collect(Collectors.joining(", ")));
+    }
+
+    private static void level(TreeNode root, List<Integer> ret) {
         Queue<TreeNode> queue = new LinkedList<>();
         queue.offer(root);
         while (!queue.isEmpty()) {
             TreeNode node = queue.poll();
-            System.out.println(node.val);
+            ret.add(node.val);
             if (node.left != null) {
                 queue.offer(node.left);
             }
             if (node.right != null) {
                 queue.offer(node.right);
             }
+        }
+    }
+
+    /**
+     * 层序遍历（迭代法）：二维数组
+     * @param root
+     */
+    public static void levelOrder2(TreeNode root) {
+        if (root == null) {
+            System.out.println("null");
+            return;
+        }
+        List<List<Integer>> ret = new ArrayList<>();
+        level2(root, ret);
+        System.out.println(ret.stream().map(l -> l.stream().map(String::valueOf).collect(Collectors.joining(", "))).collect(Collectors.joining(" | ")));
+    }
+
+    /**
+     * @param root
+     * @param ret
+     */
+    private static void level2(TreeNode root, List<List<Integer>> ret) {
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        while (!queue.isEmpty()) {
+            List<Integer> levelList = new ArrayList<>();
+            int size = queue.size();
+            while (size > 0) {
+                TreeNode node = queue.poll();
+                levelList.add(node.val);
+                if (node.left != null) {
+                    queue.offer(node.left);
+                }
+                if (node.right != null) {
+                    queue.offer(node.right);
+                }
+                size--;
+            }
+            ret.add(levelList);
         }
     }
 }
--- src/com/zmoyi/dataStructure/tree/TraversalUtilsTest.java
+++ src/com/zmoyi/dataStructure/tree/TraversalUtilsTest.java
@@ -9,17 +9,27 @@ import com.zmoyi.TreeNode;
 public class TraversalUtilsTest {
     public static void main(String[] args) {
         TreeNode root = new TreeNode(1);
-        TreeNode left = new TreeNode(2);
-        TreeNode right = new TreeNode(3);
-        root.left = left;
-        root.right = right;
+        TreeNode two = new TreeNode(2);
+        TreeNode three = new TreeNode(3);
+        TreeNode four = new TreeNode(4);
+        TreeNode five = new TreeNode(5);
+        TreeNode six = new TreeNode(6);
+        root.left = two;
+        root.right = three;
+        two.left = four;
+        two.right = five;
+        three.right = six;
 
         TraversalUtils.preOrder(root);
+        TraversalUtils.preOrderWithStack(root);
         System.out.println("~~~~~");
         TraversalUtils.inOrder(root);
+        TraversalUtils.inOrderWithStack(root);
         System.out.println("~~~~~");
         TraversalUtils.postOrder(root);
+        TraversalUtils.postOrderWithStack(root);
         System.out.println("~~~~~");
         TraversalUtils.levelOrder(root);
+        TraversalUtils.levelOrder2(root);
     }
 }
