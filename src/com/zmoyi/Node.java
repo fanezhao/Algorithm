911ef584250d8c5940f4519aac6b0be5cc321854
fanezhao <fanezhao@gmail.com>
2023-11-04 18:33:04 +0800

    mod: 填充每个节点的下一个右侧节点指针

--- src/com/zmoyi/Node.java
+++ src/com/zmoyi/Node.java
@@ -7,8 +7,13 @@ import java.util.List;
  * @date 2023/11/4 17:36
  */
 public class Node {
+
     public int val;
     public List<Node> children;
+
+    public Node left;
+    public Node right;
+    public Node next;
 
     public Node() {}
 
@@ -19,5 +24,12 @@ public class Node {
     public Node(int _val, List<Node> _children) {
         val = _val;
         children = _children;
+    }
+
+    public Node(int _val, Node _left, Node _right, Node _next) {
+        val = _val;
+        left = _left;
+        right = _right;
+        next = _next;
     }
 }
