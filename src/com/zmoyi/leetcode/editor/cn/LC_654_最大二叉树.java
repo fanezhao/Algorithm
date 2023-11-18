bab1454e6b0976c398a0a7a8e799db7b64c9040a
fanezhao <fanezhao@gmail.com>
2023-11-18 15:02:45 +0800

    mod: 最大二叉树

---
+++ src/com/zmoyi/leetcode/editor/cn/LC_654_最大二叉树.java
@@ -0,0 +1,102 @@
+//给定一个不重复的整数数组 nums 。 最大二叉树 可以用下面的算法从 nums 递归地构建: 
+//
+// 
+// 创建一个根节点，其值为 nums 中的最大值。 
+// 递归地在最大值 左边 的 子数组前缀上 构建左子树。 
+// 递归地在最大值 右边 的 子数组后缀上 构建右子树。 
+// 
+//
+// 返回 nums 构建的 最大二叉树 。 
+//
+// 
+//
+// 示例 1： 
+// 
+// 
+//输入：nums = [3,2,1,6,0,5]
+//输出：[6,3,5,null,2,0,null,null,1]
+//解释：递归调用如下所示：
+//- [3,2,1,6,0,5] 中的最大值是 6 ，左边部分是 [3,2,1] ，右边部分是 [0,5] 。
+//    - [3,2,1] 中的最大值是 3 ，左边部分是 [] ，右边部分是 [2,1] 。
+//        - 空数组，无子节点。
+//        - [2,1] 中的最大值是 2 ，左边部分是 [] ，右边部分是 [1] 。
+//            - 空数组，无子节点。
+//            - 只有一个元素，所以子节点是一个值为 1 的节点。
+//    - [0,5] 中的最大值是 5 ，左边部分是 [0] ，右边部分是 [] 。
+//        - 只有一个元素，所以子节点是一个值为 0 的节点。
+//        - 空数组，无子节点。
+// 
+//
+// 示例 2： 
+// 
+// 
+//输入：nums = [3,2,1]
+//输出：[3,null,2,null,1]
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 1 <= nums.length <= 1000 
+// 0 <= nums[i] <= 1000 
+// nums 中的所有整数 互不相同 
+// 
+//
+// Related Topics 栈 树 数组 分治 二叉树 单调栈 👍 751 👎 0
+
+
+package com.zmoyi.leetcode.editor.cn;
+
+import com.zmoyi.TreeNode;
+
+import java.util.Arrays;
+
+public class LC_654_最大二叉树 {
+    public static void main(String[] args) {
+        Solution solution = new LC_654_最大二叉树().new Solution();
+
+        int[] nums = {3, 2, 1, 6, 0, 5};
+        TreeNode root = solution.constructMaximumBinaryTree(nums);
+        System.out.println(root);
+
+    }
+    //leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode constructMaximumBinaryTree(int[] nums) {
+        if (nums.length == 0) {
+            return null;
+        }
+
+        int max = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] > nums[max]) {
+                max = i;
+            }
+        }
+
+        TreeNode root = new TreeNode(nums[max]);
+        root.left = constructMaximumBinaryTree(Arrays.copyOfRange(nums, 0, max));
+        root.right = constructMaximumBinaryTree(Arrays.copyOfRange(nums, max + 1, nums.length));
+        return root;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
