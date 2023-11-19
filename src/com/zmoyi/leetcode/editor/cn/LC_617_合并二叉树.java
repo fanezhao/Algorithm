b9ce3b37c4ebc0db990780907ee6df00857ff651
fanezhao <fanezhao@gmail.com>
2023-11-19 13:12:38 +0800

    mod: 合并二叉树 优化版本

--- src/com/zmoyi/leetcode/editor/cn/LC_617_合并二叉树.java
+++ src/com/zmoyi/leetcode/editor/cn/LC_617_合并二叉树.java
@@ -78,6 +78,26 @@ class Solution {
         root.right = mergeTrees(root1 == null ? null : root1.right, root2 == null ? null : root2.right);
         return root;
     }
+
+    /**
+     * 优化版本
+     * @param root1
+     * @param root2
+     * @return
+     */
+    public TreeNode mergeTrees2(TreeNode root1, TreeNode root2) {
+        if (root1 == null) {
+            return root2;
+        }
+        if (root2 == null) {
+            return root1;
+        }
+
+        root1.val += root2.val;
+        root1.left = mergeTrees2(root1.left, root2.left);
+        root1.right = mergeTrees2(root1.right, root2.right);
+        return root1;
+    }
 }
 //leetcode submit region end(Prohibit modification and deletion)
 
