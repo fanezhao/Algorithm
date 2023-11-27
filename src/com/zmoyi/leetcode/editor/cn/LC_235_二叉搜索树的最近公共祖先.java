6a6c432b4ca637d7e121e78a504f8f3f5cde8e6c
fanezhao <fanezhao@gmail.com>
2023-11-27 23:20:02 +0800

    mod: 二叉搜索树的最近公共祖先

---
+++ src/com/zmoyi/leetcode/editor/cn/LC_235_二叉搜索树的最近公共祖先.java
@@ -0,0 +1,105 @@
+//给定一个二叉搜索树, 找到该树中两个指定节点的最近公共祖先。 
+//
+// 百度百科中最近公共祖先的定义为：“对于有根树 T 的两个结点 p、q，最近公共祖先表示为一个结点 x，满足 x 是 p、q 的祖先且 x 的深度尽可能大（
+//一个节点也可以是它自己的祖先）。” 
+//
+// 例如，给定如下二叉搜索树: root = [6,2,8,0,4,7,9,null,null,3,5] 
+//
+// 
+//
+// 
+//
+// 示例 1: 
+//
+// 输入: root = [6,2,8,0,4,7,9,null,null,3,5], p = 2, q = 8
+//输出: 6 
+//解释: 节点 2 和节点 8 的最近公共祖先是 6。
+// 
+//
+// 示例 2: 
+//
+// 输入: root = [6,2,8,0,4,7,9,null,null,3,5], p = 2, q = 4
+//输出: 2
+//解释: 节点 2 和节点 4 的最近公共祖先是 2, 因为根据定义最近公共祖先节点可以为节点本身。 
+//
+// 
+//
+// 说明: 
+//
+// 
+// 所有节点的值都是唯一的。 
+// p、q 为不同节点且均存在于给定的二叉搜索树中。 
+// 
+//
+// Related Topics 树 深度优先搜索 二叉搜索树 二叉树 👍 1175 👎 0
+
+
+package com.zmoyi.leetcode.editor.cn;
+
+import com.zmoyi.TreeNode;
+
+public class LC_235_二叉搜索树的最近公共祖先 {
+    public static void main(String[] args) {
+        Solution solution = new LC_235_二叉搜索树的最近公共祖先().new Solution();
+    }
+    //leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+
+class Solution {
+
+    /**
+     * 个人思路
+     * @param root
+     * @param p
+     * @param q
+     * @return
+     */
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if (root == null || root == p || root == q) {
+            return root;
+        }
+
+        if (p.val > root.val && q.val > root.val) {
+            return lowestCommonAncestor(root.right, p, q);
+        } else if (p.val < root.val && q.val < root.val) {
+            return lowestCommonAncestor(root.left, p, q);
+        } else {
+            TreeNode left = lowestCommonAncestor(root.left, p, q);
+            TreeNode right = lowestCommonAncestor(root.right, p, q);
+            if (left != null && right != null) {
+                return root;
+            }
+            if (left != null) {
+                return left;
+            }
+            if (right != null) {
+                return right;
+            }
+            return null;
+        }
+    }
+
+    public TreeNode lowestCommonAncestor2(TreeNode root, TreeNode p, TreeNode q) {
+        if (root == null) {
+            return null;
+        }
+        if (p.val > root.val && q.val > root.val) {
+            return lowestCommonAncestor2(root.right, p, q);
+        }
+        if (p.val < root.val && q.val < root.val) {
+            return lowestCommonAncestor2(root.left, p, q);
+        }
+        return root;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
